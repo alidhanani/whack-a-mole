7cd73d24e604976766eac5caef3509f2f415693f
Ali Dhanani <ali.dhanani@nextauth.com>
2022-09-23 17:52:38 +0200

    background image added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BackgroundImage, Table, Image } from '@mantine/core';
 import './App.css';
 
 function App() {
+
+
+  const elements = [
+    {1: "id1", 2: "id2", 3: "id3", 4: "id4"},
+    {1: "id5", 2: "id6", 3: "id7", 4: "id8"},
+    {1: "id9", 2: "id10", 3: "id11", 4: "id12"},
+  ]
+
+  const rows = elements.map((element, index) => (
+    <tr key={"boardIndex"+index}>
+      <td key={element[1]}><Image radius="xs" width={200} height={50} src={process.env.PUBLIC_URL + '/WAM_Hole.png'} /></td>
+      <td key={element[2]}><Image radius="xs" width={200} height={50} src={process.env.PUBLIC_URL + '/WAM_Hole.png'} /></td>
+      <td key={element[3]}><Image radius="xs" width={200} height={50} src={process.env.PUBLIC_URL + '/WAM_Hole.png'} /></td>
+      <td key={element[4]}><Image radius="xs" width={200} height={50} src={process.env.PUBLIC_URL + '/WAM_Hole.png'} /></td>
+    </tr>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BackgroundImage
+      src={process.env.PUBLIC_URL + '/WAM_bg.jpg'}
+      style={{height: window.innerHeight}}
+      >
+        <Table>
+          <tbody>
+            {rows}
+          </tbody>
+        </Table>
+      </BackgroundImage>
   );
 }
 
