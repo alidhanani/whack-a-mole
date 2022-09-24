2b5191c330078bbe79bc018a711e389c630e29bf
Ali Dhanani <ali.dhanani@nextauth.com>
2022-09-25 00:38:02 +0200

    Score, Timer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import { BackgroundImage, Center, Image, Text } from '@mantine/core';
+import { BackgroundImage, Button, Center, Image, Modal, Text } from '@mantine/core';
 import { useEffect, useState } from 'react';
+import { useTimer } from 'react-timer-hook';
 import './App.css';
 
 function App() {
+
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 100);
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({expiryTimestamp: time, onExpire: () => setOpened(true)});
+
 
   const HOLE = '/WAM_Hole.png'
   const MOLE = '/WAM_Mole.png'
 
   const [pic, setPic] = useState<string[]>([HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE]);
   const [score, setScore] = useState<number>(0)
+  const [opened, setOpened] = useState(false);
 
   const rowElem1 = [0, 1, 2, 3]
   const rowElem2 = [4, 5, 6, 7]
@@ -18,20 +35,16 @@ function App() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const updateValue = () => {
-    let guessNumber = randomNumberInRange(0, 11)
-    var newArray = [HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE]
-    newArray[guessNumber] = MOLE
-    setPic(newArray)
-  }
-
   useEffect(() => {
-    setInterval(
-      updateValue,
-      2000
-    );
-  },[pic, updateValue])
+    var newArray = [HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE,HOLE]
+    function updateValue(){
+      let guessNumber = randomNumberInRange(0, 11)
+      newArray[guessNumber] = MOLE
+      setPic(newArray)
+    }
+    const interval = setInterval(updateValue, 3000);
+    return () => clearInterval(interval);
+  },[pic])
 
   const onArrayClick = (event: any) => {
     if(event.target.src === ("http://localhost:3000" + MOLE)) {
@@ -43,26 +56,47 @@ function App() {
   }
 
   const rowElements1 = rowElem1.map((element, index) => (
-      <td onClick={onArrayClick} key={"Data"+element}><Image radius="xs" width={200} height={50} src={process.env.PUBLIC_URL + pic[element]} /></td>
+      <td height={150} onClick={onArrayClick} key={"Data"+element}><Image radius="xs" width={200} height={pic[element] === MOLE ? 150 : 50} src={process.env.PUBLIC_URL + pic[element]} /></td>
   ));
 
   const rowElements2 = rowElem2.map((element, index) => (
-      <td onClick={onArrayClick} key={"Data"+element}><Image radius="xs" width={200} height={50} src={process.env.PUBLIC_URL + pic[element]} /></td>
+      <td height={150}  onClick={onArrayClick} key={"Data"+element}><Image radius="xs" width={200} height={pic[element] === MOLE ? 150 : 50} src={process.env.PUBLIC_URL + pic[element]} /></td>
   ));
 
   const rowElements3 = rowElem3.map((element, index) => (
-    <td onClick={onArrayClick} key={"Data"+element}><Image radius="xs" width={200} height={50} src={process.env.PUBLIC_URL + pic[element]} /></td>
+    <td  height={150} onClick={onArrayClick} key={"Data"+element}><Image radius="xs" width={200} height={pic[element] === MOLE ? 150 : 50} src={process.env.PUBLIC_URL + pic[element]} /></td>
   ));
 
 
   return (
       <BackgroundImage
       src={process.env.PUBLIC_URL + '/WAM_bg.jpg'}
-      style={{height: window.innerHeight}}
+      style={{height: window.innerHeight, cursor:'url(http://localhost:3000/WAM_Hammer.png),auto'}}
       >
-        <Text>Success Score:{score}</Text>
+        <Modal
+          opened={opened}
+          onClose={() => setOpened(false)}
+          title="Introduce yourself!"
+        >
+          <table>
+            <tbody>
+              <tr>
+              </tr>
+            </tbody>
+          </table>
+          <Button onClick={() => {
+            setScore(0)
+            restart(time)
+            setOpened(false)
+          }} >Restart</Button>
+        </Modal>
+        <div style={{padding: '0px'}}>
+          <Text align="left" size={24} weight="bold" color="red" >Score</Text>
+          <Text align="left" weight={700} size={30} >{score}</Text>
+          <Text align="center" size={24} weight="bold" color="red" ><span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span></Text>
+        </div>
         <Center>
-        <table style={{paddingTop: window.innerHeight / 5}}>
+        <table>
           <tbody>
             <tr key={"boardIndex1"}>
               {rowElements1}
